1417824a9c900d2f552bd13574491d697bf83714
nikhil <nikhilvaidyar1997@gmail.com>
2020-06-09 09:10:38 +0530

    Initializing doubly linked list

---
+++ LinkedList/DoublyLinkedList.java
@@ -0,0 +1,30 @@
+// Doubly Linked List Node
+
+public class DoublyLinkedList<T> implements Iterable<T> {
+    private int size = 0;
+    private Node<T> head = null;
+    private Node<T> tail = null;
+
+    // Internal node class to load data
+    private class Node<T> {
+        T data;
+        Node<T> prev, next;
+
+        public Node(T data, Node<T> prev, Node<T> next) {
+            this.data = data;
+            this.prev = prev;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return data.toString();
+        }
+    }
+
+    // Empty this linked list, O(n)
+    public void clear(){
+        
+    }
+
+}
