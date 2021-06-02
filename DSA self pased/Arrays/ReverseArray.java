78dfa2f006c04bafe6aae0a614d98ef19598e74c
Nikhil Vaidyar <nikhil.vaidyar@biofourmis.com>
2021-06-02 09:33:38 +0530

    added more array ds problems

--- DSA self pased/Arrays/ReverseArray.java
+++ DSA self pased/Arrays/ReverseArray.java
@@ -6,17 +6,28 @@ public class ReverseArray {
     static void reverse(int arr[], int n) {
         int low = 0, high = n - 1;
         while (low < high) {
-            int temp = arr[low];
-            arr[low] = arr[high];
-            arr[high] = temp;
+            swap(arr[low], arr[high]);
+            // int temp = arr[low];
+            // arr[low] = arr[high];
+            // arr[high] = temp;
             low++;
             high--;
         }
     }
 
+    static void swap(int low, int high) {
+        int temp = low;
+        low = high;
+        high = temp;
+    }
+
     public static void main(String[] args) {
         int arr[] = { 1, 2, 3, 5 };
         int n = arr.length;
-        System.out.println(reverse(arr, n));
+        reverse(arr, n);
+        System.out.println("After reversing the array");
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
     }
 }
--- DSA self pased/Arrays/RotateLeftByOneArray.java
+++ DSA self pased/Arrays/RotateLeftByOneArray.java
@@ -1,19 +1,19 @@
-// // Time Complexity - O(n)
-// // Space Complexity - O(1)
-// public class RotateLeftByOneArray {
-// public static void rotateLeft(int arr[], int n) {
-// // Create a temporary array
-// int temp = arr[0];
-// // Traverse over the array and change the index value from first to next
-// for (int i = 1; i < n; i++) {
-// arr[i - 1] = arr[i];
-// }
-// arr[n - 1] = temp;
-// }
+public class RotateLeftByOneArray {
+    static void rotateNaive(int arr[]) {
+        int n = arr.length;
+        int temp = arr[0];
+        for (int i = 1; i < n; i++) {
+            arr[i - 1] = arr[i];
+        }
+        arr[n - 1] = temp;
+    }
 
-// public static void main(String[] args) {
-// int arr[] = { 4, 5, 6, 7, 8 };
-// int n = arr.length;
-// System.out.println(rotateLeft(arr, n));
-// }
-// }
+    public static void main(String[] args) {
+        int arr[] = { 55, 1, 2, 3, 4, 5 };
+        rotateNaive(arr);
+        System.out.println("After rotating to left");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+}
